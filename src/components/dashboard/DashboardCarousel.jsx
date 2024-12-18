ad7d19d7a71f3af2926355bf8054725603526124
Abhishek Vaze <abhishek.vaze.professional@gmail.com>
2024-12-18 10:08:55 +0530

    removed dashboard carousel which was causing the issue

--- src/components/dashboard/DashboardCarousel.jsx
+++ src/components/dashboard/DashboardCarousel.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import Carousel from "better-react-carousel";
 import photoData from "../../data/dashboardPics.json";
-import useImage from "../common/useImage";
 import { useMediaQuery, useTheme } from "@mui/material";
 function DashboardCarousel() {
   const photos = photoData.dashPics;
@@ -9,29 +7,6 @@ function DashboardCarousel() {
   const isMatch = useMediaQuery(theme.breakpoints.down("md"));
   return (
     <>
-      {isMatch ? (
-        <Carousel cols={1} rows={1} gap={10} showDots={true} autoplay={3000} loop>
-          {photos.map((object, i) => {
-            const { loading, error, image } = useImage(object.img, "photoes");
-            return (
-              <Carousel.Item>
-                <img src={image} className='dashimg-mob' />
-              </Carousel.Item>
-            );
-          })}
-        </Carousel>
-      ) : (
-        <Carousel cols={3} rows={1} gap={1} showDots={true} autoplay={4000} loop>
-          {photos.map((object, i) => {
-            const { loading, error, image } = useImage(object.img, "photoes");
-            return (
-              <Carousel.Item>
-                <img src={image} className='dashimg' />
-              </Carousel.Item>
-            );
-          })}
-        </Carousel>
-      )}
     </>
   );
 }
