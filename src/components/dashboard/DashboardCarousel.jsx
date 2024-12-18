1dc2cc10a7858a3821acccaec7858e4ed6d85b87
Abhishek Vaze <abhishek.vaze.professional@gmail.com>
2024-12-18 11:25:18 +0530

    added dashboard carousel

--- src/components/dashboard/DashboardCarousel.jsx
+++ src/components/dashboard/DashboardCarousel.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 import photoData from "../../data/dashboardPics.json";
+import useImage from "../common/useImage";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 import { useMediaQuery, useTheme } from "@mui/material";
+import building1 from "../../assets/images/Picture1.jpg";
 function DashboardCarousel() {
   const photos = photoData.dashPics;
   const theme = useTheme();
   const isMatch = useMediaQuery(theme.breakpoints.down("md"));
   return (
     <>
+      <Carousel
+        autoPlay={true}
+        interval={3000}
+        infiniteLoop={true}
+        showThumbs={false}
+      >
+        {photos.map((object, i) => {
+          const { loading, error, image } = useImage(object.img, "photoes");
+          return (
+            <div>
+              <img src={image} className="dashimg" />
+            </div>
+          );
+        })}
+      </Carousel>
     </>
   );
 }
