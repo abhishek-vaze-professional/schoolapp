af232b774f4fa40672050a7d3ebe8a02c33550bb
abhishekvaze87 <abhishek.vaze.professional@gmail.com>
2025-01-16 18:32:06 +0530

    added achievment pic

--- src/components/achievement/Achievement.jsx
+++ src/components/achievement/Achievement.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import AchievementTab from "./AchievementTab";
 import { Box } from "@mui/material";
 import { whiteBoxStyle } from "../common/commonUtil";
+import staffpic from "../../assets/images/achievementpic.jpg";
 function Achievement() {
   return (
     <div className="capriolafont textColorGray">
       <p className="sHeader" id="head">Achievements</p>
       <br></br>
+      <Box className='content whiteText' sx={whiteBoxStyle}>
+          <img src={staffpic} className="staffimg" />
+        </Box>
       <Box className="content textColorGray" sx={whiteBoxStyle}>
         <AchievementTab></AchievementTab>
       </Box>
